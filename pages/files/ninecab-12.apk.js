9c0fb242e809452253ac59324f0b2a8611b5bf4e
Emmanuel Nosakhare <nonreactiv@gmail.com>
2018-07-16 15:29:17 +0100

    Added ninecab-12.apk

---
+++ pages/files/ninecab-12.apk.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default class NineCab12 extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            httpEquiv="refresh"
+            content="2;url=https://www.dropbox.com/s/l3b1kixsdovkgus/ninecab-12.apk?dl=1"
+          />
+        </Head>
+      </React.Fragment>
+    )
+  }
+}
