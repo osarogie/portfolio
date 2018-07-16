490d3c02982d141d644ef44708db263124b3ce21
Emmanuel Nosakhare <nonreactiv@gmail.com>
2018-07-16 15:36:23 +0100

    Added title and description

--- pages/files/ninecab-12.apk.js
+++ pages/files/ninecab-12.apk.js
@@ -7,6 +7,11 @@ export default class NineCab12 extends React.Component {
       <React.Fragment>
         <Head>
           <meta charSet="utf-8" />
+          <title>9Cab - Build 12</title>
+          <meta
+            name="description"
+            content="Download build 12 of the 9 Cab app"
+          />
           <meta
             httpEquiv="refresh"
             content="2;url=https://www.dropbox.com/s/l3b1kixsdovkgus/ninecab-12.apk?dl=1"
